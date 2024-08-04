b236ea9a5e0b6b2544825120533fdd5ffd029be1
giri <mgiricharan@email.com>
2024-08-04 17:13:35 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,27 @@ const PORT = process.env.PORT;
 // Database connection
 dbConnect();
 
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+// Update CORS settings
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://giri-todo-app.onrender.com",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin like mobile apps or curl requests
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 
 // router
 app.use("/api", router);
